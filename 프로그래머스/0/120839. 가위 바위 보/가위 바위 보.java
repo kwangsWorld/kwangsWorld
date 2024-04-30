4d14cb1cfed2afd9255a5419a86f434c657e0a96
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-04-30 19:14:09 +0900

    [level 0] Title: 가위 바위 보, Time: 2.96 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        char[] array = rsp.toCharArray();
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == '0'){
+                answer += '5';
+            }else if(array[i] == '2'){
+                answer += '0';
+            }else{
+                answer += '2';
+            }
+        }
+        return answer;
+    }
+}
