7d264d1775c9d7851d1aaf98837ac3fb7a57fe4c
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-05-06 20:10:21 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int[] box, int n) {
         int answer = 0;
-        int cuboid = box[0] * box[1] * box[2];
         answer = (box[0] / n) * (box[1] / n) * (box[2] / n);
         return answer;
     }
