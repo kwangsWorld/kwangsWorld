7bb42d740a565dd33052732365f18465ce990ceb
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-06-02 00:23:55 +0900

    [level 0] Title: 369게임, Time: 0.19 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String num = Integer.toString(order);
+        for(int i = 0; i < num.length(); i++){
+            System.out.println(num.charAt(i));
+            int judge = (int)num.charAt(i);
+            if(judge == 51 || judge == 54 || judge == 57){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
