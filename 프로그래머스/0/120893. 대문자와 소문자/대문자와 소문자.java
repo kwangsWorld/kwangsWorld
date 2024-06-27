57e23078909b45ef81498047aafa958cd90542b1
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-06-27 16:08:18 +0900

    [level 0] Title: 대문자와 소문자, Time: 13.39 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            int c = my_string.charAt(i);
+            if(Character.isUpperCase(c)){
+                answer += (char)Character.toLowerCase(c);
+            }else{
+                answer += (char)Character.toUpperCase(c);
+            }
+        }
+        return answer;
+    }
+}
