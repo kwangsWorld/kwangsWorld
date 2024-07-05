67b4131259fd5a821a50c1f2a4063cba315b94e7
kwangsWorld <43844533+kwangsWorld@users.noreply.github.com>
2024-07-05 17:41:26 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] strArray = my_string.split(" ");
+        
+        answer += Integer.parseInt(strArray[0]);
+        
+        for(int i = 1; i < strArray.length-1; i++){
+            if(strArray[i].equals("+")){
+                answer += Integer.parseInt(strArray[i+1]);
+            }else if(strArray[i].equals("-")){
+                answer -= Integer.parseInt(strArray[i+1]);
+            }
+        }
+        return answer;
+    }
+}
